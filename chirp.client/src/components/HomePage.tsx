351a587a9b9c365e03a1322864dfc1bf8664c294
Enoch Whitaker <enochwhitaker3@gmail.com>
2024-11-08 15:49:10 -0700

    Style updates

--- chirp.client/src/components/HomePage.tsx
+++ chirp.client/src/components/HomePage.tsx
@@ -1,21 +1,26 @@
-import { UserAccountQueries } from "../hooks/UserAccountQueries";
-import LoginButton from "./authentication/LoginButton";
+import Homesvg from "../assets/homesvg";
 
 const HomePage = () => {
-  const { data: Users } = UserAccountQueries.useGetAllUsers();
+  // const { data: Users } = UserAccountQueries.useGetAllUsers();
+  {
+    /* <h1 id="tabelLabel" className="text-red-500">
+    Users
+  </h1>
+  <ul>
+    {Users?.map((user) => (
+      <li key={user.id}>
+        {user.id} - {user.username}
+      </li>
+    ))}
+  </ul>
+  <LoginButton /> */
+  }
   return (
     <div>
-      <h1 id="tabelLabel" className="text-red-500">
-        Users
-      </h1>
-      <ul>
-        {Users?.map((user) => (
-          <li key={user.id}>
-            {user.id} - {user.username}
-          </li>
-        ))}
-      </ul>
-      <LoginButton />
+      <div className="flex flex-row">
+        <Homesvg />
+        <p className="text-white text-xl">Home</p>
+      </div>
     </div>
   );
 };
