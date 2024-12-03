49fc4713854c6e47c93e75e370e518ed86f13726
Enoch Whitaker <enochwhitaker3@gmail.com>
2024-12-03 10:47:51 -0700

    Capped friends display on right sidebar

--- chirp.client/src/components/layout/RightSidebar.tsx
+++ chirp.client/src/components/layout/RightSidebar.tsx
@@ -11,12 +11,17 @@ const RightSidebar = () => {
   ) as UserAccountContextInterface;
 
   const { data: following } = FollowQueries.useGetFollowingByUserId(user?.id!);
+  const limitedFollowing = following?.slice(0, 4);
 
   return (
     <div className="avg:flex avg:justify-center hidden">
       <div className="flex flex-col justify-between items-end desk:w-[300px] w-[200px] h-96 sticky top-20">
         <div className="flex flex-col justify-start items-start">
-          <Friends user={user} following={following} isLoading={isLoading} />
+          <Friends
+            user={user}
+            following={limitedFollowing}
+            isLoading={isLoading}
+          />
           <p className="dark:text-white text-black font-bold text-xl mt-10">
             Mode
           </p>
