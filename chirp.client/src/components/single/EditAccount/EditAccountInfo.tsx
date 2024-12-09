3cb1ac33374a990b5ed8deddbbf18d8c61fd7544
Enoch Whitaker <enochwhitaker3@gmail.com>
2024-12-09 12:40:58 -0700

    Small fix for linting

--- chirp.client/src/components/single/EditAccount/EditAccountInfo.tsx
+++ chirp.client/src/components/single/EditAccount/EditAccountInfo.tsx
@@ -4,7 +4,7 @@ import EditField from "./EditField";
 const EditAccountInfo = ({ User }: { User: UserAccount }) => {
   return (
     <div className="flex flex-col dark:text-white text-black my-4 mobile:mx-0 mx-4 w-full">
-        <EditField
+      <EditField
         label={"Username"}
         field={User.username}
         maxLength={25}
